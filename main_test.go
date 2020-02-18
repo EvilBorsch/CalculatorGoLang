e5c2e290118fe66eccd8de67dd5872dddfcc1b92
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-02-18 20:35:51 +0300

    Добавил нормальные тесты

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
+func TestCalc(t *testing.T) {
 	assert := assert.New(t)
 
 	bigData := "1+2*(3+4/2-(1+2))*2+1"
@@ -39,5 +39,61 @@ func TestHello(t *testing.T) {
 	twoBrackets = "(30-40)*2+(200-500)/40"
 	result2 := calc(twoBrackets)
 	assert.Equal(result, result2)
+
+}
+
+func TestIsDigit(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(isDigit("2"), true)
+	assert.Equal(isDigit("a"), false)
+}
+
+func TestFactorize(t *testing.T) {
+	assert := assert.New(t)
+	var res = []string{"2", "+", "3", "*", "4"}
+	assert.Equal(factorize("2+3*4"), res)
+
+	res = []string{"20", "+", "30", "*", "40"}
+	assert.Equal(factorize("20+30*40"), res)
+}
+
+func TestConvert(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(FloatToString(2.05), "2.050000")
+	assert.Equal(StringToFloat("3.0"), float64(3.0))
+	assert.Equal(StringToFloat("a"), float64(0))
+	assert.Equal(StringToFloat("qweajskhdkjashdkjas"), float64(0))
+	assert.Equal(FloatToString(123), "123.000000")
+
+}
+
+func TestElementCalc(t *testing.T) {
+	assert := assert.New(t)
+
+	operationStack := make(stack, 0)
+	numbersStack := make(stack, 0)
+
+	operationStack = operationStack.Push("+")
+	numbersStack = numbersStack.Push("2").Push("2")
+
+	operationStack, numbersStack = calcElement(operationStack, numbersStack)
+
+	numbersStack, res, _ := numbersStack.Pop()
+	assert.Equal(res, FloatToString(float64(4)))
+	assert.Equal(operationStack.isEmpty(), true)
+
+	operationStack2 := make(stack, 0)
+	numbersStack2 := make(stack, 0)
+
+	operationStack2 = operationStack.Push("+").Push("*")
+	numbersStack2 = numbersStack.Push("2").Push("3").Push("4").Push("5")
+
+	operationStack2, numbersStack2 = calcElement(operationStack2, numbersStack2)
+
+	numbersStack2, lastNum, _ := numbersStack2.Pop()
+	assert.Equal(lastNum, FloatToString(float64(20)))
+
+	operationStack2, lastOperation, _ := operationStack2.Pop()
+	assert.Equal(lastOperation, "+")
 
 }
